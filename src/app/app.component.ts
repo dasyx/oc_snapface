170b26f47047c148e91451ff3970c01aaab59369
dasyx <seanpnn00@gmail.com>
2024-09-18 23:37:48 +0200

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
-import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+//import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
 import { HeaderComponent } from "./header/header.component";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root", // Déclare le sélecteur pour ce composant, utilisé dans le fichier index.html ou autre template parent
   standalone: true, // Indique que ce composant est autonome et n'a pas de dépendances supplémentaires
-  imports: [FaceSnapListComponent, HeaderComponent], // Indique que le composant FaceSnapComponent est utilisé dans ce composant parent
+  imports: [HeaderComponent, RouterOutlet], // Indique que le composant FaceSnapComponent est utilisé dans ce composant parent
   templateUrl: "./app.component.html", // Lien vers le template HTML associé à ce composant
   styleUrl: "./app.component.scss", // Lien vers le fichier de style SCSS associé à ce composant
 })

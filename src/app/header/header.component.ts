5d7a8694ea9226ad4b6cc5249ea10d4b75363aac
dasyx <seanpnn00@gmail.com>
2024-09-19 23:19:02 +0200

    add routerlink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
+  selector: "app-header",
   standalone: true,
-  imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.scss",
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
